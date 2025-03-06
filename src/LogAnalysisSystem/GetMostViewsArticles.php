fae5e1cf85f84c7ccdc8c81747699772a2687add
takuya <cdwbh564@yahoo.co.jp>
2025-03-07 01:21:27 +0900

    feat: 指定数の人気記事をビュー数順に取得し、ドメインコード・タイトル・ビュー数を表示

---
+++ src/LogAnalysisSystem/GetMostViewsArticles.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LogAnalysisSystem;
+
+use LogAnalysisSystem\MenuAction;
+use PDO;
+
+class GetMostViewsArticles implements MenuAction
+{
+
+    public function executeMenu(PDO $pdo): array
+    {
+
+        // ビュー数が多い記事を取得する SQL 文
+        $sql = 'SELECT domain_code, page_title, view_count
+        FROM
+            wiki_pageview_raw
+        ORDER BY
+            view_count DESC
+        LIMIT :articlesNum';
+
+        // SQL をプリペアドステートメントとして準備
+        $sth = $pdo->prepare($sql);
+
+        // 取得する記事の数を設定
+        $articlesNum = 3;
+        $sth->bindValue(':articlesNum', $articlesNum, PDO::PARAM_INT); // バインド
+
+        // SQL を実行
+        $sth->execute();
+
+        // 実行結果をすべて取得（連想配列として）
+        $pageViews = $sth->fetchAll();
+        return $pageViews;
+    }
+}
