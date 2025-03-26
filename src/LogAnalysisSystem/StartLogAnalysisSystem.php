64cc90a0deff62371209fba9882450be0be8cda6
takuya <cdwbh564@yahoo.co.jp>
2025-03-27 01:35:49 +0900

    feat:ユーザー入力をバリデーション

--- src/LogAnalysisSystem/StartLogAnalysisSystem.php
+++ src/LogAnalysisSystem/StartLogAnalysisSystem.php
@@ -9,13 +9,24 @@ $pdo = new DBConnector();
 $pdo = $pdo->getPDO();
 
 // ユーザーにメニューを選択させる処理
-echo 'メニューを選択して下さい' . PHP_EOL;
+echo 'メニュー番号を選択して下さい' . PHP_EOL;
 echo '1.ビュー数の多い記事を検索' . PHP_EOL;
 echo '2.ドメインコードの中で人気な記事を検索' . PHP_EOL;
+// TODO;メニューから抜ける選択肢実装
+// echo '9.終了する' . PHP_EOL;
 
-$selectedMenu = (int)trim(fgets(STDIN));
+$selectedMenu = trim(fgets(STDIN));
+// 文字列を
+if (!ctype_digit($selectedMenu)) {
+    exit('メニュー番号は半角英数字で入力して下さい。' . PHP_EOL);
+};
 
-$menu = MenuFactory::createClass($selectedMenu);
+// 対応していないメニュー番号の場合、matct式によりスロー発生
+try {
+    $menu = MenuFactory::createClass($selectedMenu);
+} catch (UnhandledMatchError $e) {
+    exit('対応していないメニュー番号です。' . PHP_EOL);
+}
 
 // データを取得
 $menu->executeMenu($pdo);
