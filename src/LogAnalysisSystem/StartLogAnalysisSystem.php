2dab3af002cf6c44f60085e15939d3525852e66c
takuya <cdwbh564@yahoo.co.jp>
2025-03-29 22:45:44 +0900

    refactor: ユーザー入力に対する数字判定のバリデーション処理を追加

--- src/LogAnalysisSystem/StartLogAnalysisSystem.php
+++ src/LogAnalysisSystem/StartLogAnalysisSystem.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use LogAnalysisSystem\MenuFactory;
 use LogAnalysisSystem\DBConnector;
+use LogAnalysisSystem\Validator;
 
 ini_set('log_errors', 'On');
 ini_set('error_log', __DIR__ . '/log/error.log');
@@ -19,15 +20,19 @@ echo '2.ドメインコードの中で人気な記事を検索' . PHP_EOL;
 // TODO;メニューから抜ける選択肢実装
 // echo '9.終了する' . PHP_EOL;
 
+// メニューの選択
 $selectedMenu = trim(fgets(STDIN));
 // 数字のみの文字列か判定
-if (!ctype_digit($selectedMenu)) {
-    exit('メニュー番号は数字で入力して下さい。' . PHP_EOL);
+$validator = new Validator;
+$return = $validator->validateNumeric($selectedMenu);
+// 数字以外の入力の場合は終了
+if ($return) {
+    exit;
 };
 
+// 選択番号に応じてインスタンス作成
 // 対応していないメニュー番号の場合、matct式によりスロー発生
 try {
-    // 選択番号に応じてインスタンス作成
     $menu = MenuFactory::createClass($selectedMenu);
 } catch (UnhandledMatchError $e) {
     exit('対応していないメニュー番号です。' . PHP_EOL);
@@ -40,7 +45,7 @@ try {
     // スタックトレースでエラーの発生行など確認
     error_log($e->getMessage() . PHP_EOL . $e->getTraceAsString());
     echo 'DBエラーが発生しました。' . PHP_EOL;
-    exit(1);
+    exit;
 }
 
 // 取得データを出力
